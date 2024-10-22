79fd33a3c78ff4bb7c12051c40d24f4fb888e284
ElZapallo432 <jpbm432@gmail.com>
2024-10-22 11:23:26 -0300

    probando

---
+++ database/database.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client[os.getenv("DATABASE_NAME")]
+ramos_collection = db["ramos"]
+ratings_collection = db["ratings"]
