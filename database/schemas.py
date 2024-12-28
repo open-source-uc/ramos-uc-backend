0d3e35d18cb243e92de0a9d3ca10b4cd1f97699e
Utmite <rockimunoz94@gmail.com>
2024-12-27 22:00:30 -0300

    mejoras

--- database/schemas.py
+++ database/schemas.py
@@ -1,10 +1,23 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import List
+from enum import Enum
 
 class Rate(BaseModel):
     rating: bool  # Booleano para que sea como bueno o malo (True = bueno, False = malo)
     comment: str  # Comentario de la reseña
+    profesor: str
+    year: int
+    semestre: int | str
     user_id: str  # ID del usuario que dejó la reseña
+    creditos_presuntos: int # Los creditos que pensas que valen x ejemplo BBDD vale 10 pero es como 30
+
+    @field_validator('semestre')
+    def validate_semestre(cls, value):
+        if value in ["TAV", 1, 2]:
+            return value
+        else:
+            raise ValueError(f"El valor numérico {value} no corresponde a un semestre válido.")
+       
 
 class Ramo(BaseModel):
     sigle: str
