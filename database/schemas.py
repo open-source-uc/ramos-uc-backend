19ecac8b250a448f166ed422bb7a3be2be4d32f7
MeuDeuz <juguete2005@gmail.com>
2024-12-29 18:19:53 -0300

    avance login :v

--- database/schemas.py
+++ database/schemas.py
@@ -14,7 +14,6 @@ class Rate(BaseModel):
 
 #User
 class Login(BaseModel):
-    username: str
     email: EmailStr
     password: str
 
--- routes/login.py
+++ routes/login.py
@@ -8,11 +8,21 @@ from lib.auth.encrypt import encriptar_password, validacion_password
 
 router = APIRouter()
 
-@router.post("/accounts")
+@router.post("/accounts/register")
 async def create_usr(email: str, password: str):
-    if accounts_collection.find_one({"email": email}):
-        raise HTTPException("Usuario ya existente")
+    if accounts_collection.find_one({"email": email}) is not None:
+        raise HTTPException("Usuario ya existente", status_code=409)
     hashed_password = encriptar_password(password)
     usuario = Login(email, hashed_password)
     accounts_collection.insert_one(usuario)
+    return JSONResponse({"message": "Ok"})
+
+@router.post("/accounts/login")
+async def login_usr(email: str, password: str):
+    account = accounts_collection.find_one({"email": email})
+    if not account:
+        raise HTTPException("Usuario no existe", status_code=404)
+    account = Login(**account)
+    if not validacion_password(password, account.password):
+        raise HTTPException("La contraseña no es correcta", status_code=401)
     return JSONResponse({"message": "Ok"})
